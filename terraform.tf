8f063c96b764b77a214be79728da1010448bdd86
Masahiro Ishii <marufeuille@gmail.com>
2019-03-04 02:35:41 +0000

    adding first tf

---
+++ terraform.tf
@@ -0,0 +1,35 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "zone" {}
+
+provider "alicloud" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+  region = "${var.region}"
+}
+
+resource "alicloud_vpc" "vpc" {
+  name = "terraform-vpc"
+  cidr_block = "192.168.1.0/24"
+}
+
+resource "alicloud_vswitch" "vsw" {
+  vpc_id            = "${alicloud_vpc.vpc.id}"
+  cidr_block        = "192.168.1.0/28"
+  availability_zone = "${var.zone}"
+}
+
+resource "alicloud_cs_kubernetes" "main" {
+  name_prefix = "terraform-k8s"
+  availability_zone = "${alicloud_vswitch.vsw.availability_zone}"
+  new_nat_gateway = true
+  master_instance_types = ["ecs.n4.small"]
+  worker_instance_types = ["ecs.n4.small"]
+  worker_numbers = [1]
+  key_name = ""
+  pod_cidr = "192.168.1.0/24"
+  service_cidr = "192.168.2.0/24"
+  enable_ssh = true
+  install_cloud_monitor = true
+}
